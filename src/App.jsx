e1ac35d50713a9889ee7b5ba00b374a18343029b
chimaobi nwaokoro <chimaobinwaokoro27@gmail.com>
2026-08-19 01:52:16 +0100

    update App.jsx and add qeuetion 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import one from "./componet/one";
+import Question2 from "./componet/two";
 function App() {
   return (
     <div>
       <one />
+      <Question2 />
     </div>
   );
 }
---
+++ src/componet/qeuetion 2.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function Question2() {
+  const [number, setNumber] = useState(0);
+
+  function increaseNumber() {
+    setNumber(number + 1);
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-950">
+      <div className="w-80 rounded-2xl bg-slate-900 p-8 text-center shadow-2xl">
+        <h1 className="mb-2 text-2xl font-bold text-white">
+          Question2: Counter
+        </h1>
+
+        <p className="mb-6 text-6xl font-bold text-white">
+          {number}
+        </p>
+
+        <button
+          onClick={increaseNumber}
+          className="w-full rounded-xl bg-green-600 px-6 py-3 text-lg font-semibold text-white transition hover:bg-green-700 active:scale-95"
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Question2;
