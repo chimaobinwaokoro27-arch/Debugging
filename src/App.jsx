af90a47536c26fd2dd12190e2191414388df6d5a
chimaobi nwaokoro <chimaobinwaokoro27@gmail.com>
2026-08-19 01:57:47 +0100

    update App.jsx, add question2 and question 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import one from "./componet/one";
-import Question2 from "./componet/two";
+import Question2 from "./componet/qeuetion2";
+
 function App() {
   return (
     <div>
       <one />
       <Question2 />
+      <Question3 />
     </div>
   );
 }
